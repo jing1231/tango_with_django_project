ddaa1daab10d3204bc22d6737d77c53d367b3409
Jing Xue <2640544x@student.gla.ac.uk>
2022-02-06 20:06:25 +0000

    pass chapter 4 test

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage':'Crunchy, creamy,cookie,candy,cupcake'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html',context=context_dict)
     # return HttpResponse("Rango says here is the about page!")
 
@@ -11,3 +11,6 @@ def about(request):
     # return HttpResponse("Rango says here is the about page.")
     context_dict = {'boldmessage':'This tutorial has been put together by Jing Xue'}
     return render(request, 'rango/about.html',context=context_dict)
+
+
+
