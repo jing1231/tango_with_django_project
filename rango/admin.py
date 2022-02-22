adb65b8838007bf39f61a68ff7b5a950996d6431
Jing Xue <2640544x@student.gla.ac.uk>
2022-02-22 22:45:05 +0000

    chapter9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page 
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
@@ -14,4 +15,6 @@ admin.site.register(Page,PageAdmin)
 
 # including this customised interface
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(UserProfile)
 
